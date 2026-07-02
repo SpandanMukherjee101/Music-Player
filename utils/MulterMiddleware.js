59f12b7313f2dc89078f41569dfce6337466c312
Spandan Mukherjee <tandram2013@gmail.com>
2026-07-02 11:22:45 +0530

    Refactor Busboy import to simplify usage and remove unnecessary variable

--- utils/MulterMiddleware.js
+++ utils/MulterMiddleware.js
@@ -1,7 +1,5 @@
-﻿const BusboyPackage = require("busboy")
+﻿const Busboy = require("busboy")
 const cloudinary = require("cloudinary").v2
-
-const Busboy = BusboyPackage?.Busboy || BusboyPackage
 
 module.exports = (req, res, next) => {
     if (!req.headers["content-type"] || !req.headers["content-type"].includes("multipart/form-data")) {
