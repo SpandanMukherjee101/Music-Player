cd6bb6ee92b64c74d62acd7729b91a4fccf5932c
Spandan Mukherjee <tandram2013@gmail.com>
2026-07-02 13:13:55 +0530

    feat: add JWT authentication middleware to verify user tokens

--- middlewares/AuthVerify.js
+++ middlewares/AuthVerify.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
     const authHeader = req.headers.authorization
     const token = authHeader?.startsWith("Bearer ")
         ? authHeader.split(" ")[1]
-        : req.headers.token || req.headers["x-auth-token"]
+        : req.headers.token || req.headers["x-auth-token"] || req.query?.token
 
     if (!token) {
         return res.status(401).json({ message: "Token is required" })
